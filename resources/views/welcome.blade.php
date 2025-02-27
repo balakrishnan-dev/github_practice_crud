7427859da44f4f639de33b27a6da71e9d3ea4d3d
balakrishnan-dev <balakrishnan0tech@gmail.com>
2025-02-27 23:56:35 +0530

    dd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>lk
+<body>
     <h1>name : balakrishnan</h1>
 </body>
 </html>
